242243d74d75b937f7557369f670a83a1cdb9d61
Kevin Garner <kevin@kevingarner.net>
2022-05-10 20:30:27 -0500

    basic movement "demo" complete

--- main.cpp
+++ main.cpp
@@ -8,11 +8,29 @@ int main() {
   terminal_open();
 
   terminal_bkcolor(color_from_name("grey"));
-  terminal_clear();
-  terminal_print(1, 1, "Hello, world!");
-  terminal_refresh();
 
-  while(terminal_read() != TK_CLOSE);
+  int x = 5;
+  int y = 5;
+
+  bool quit = false;
+  while(!quit) {
+    terminal_clear();
+    terminal_color(color_from_name("green"));
+    terminal_put(x, y, '@');
+    terminal_refresh();
+    int k = terminal_read();
+    quit = k == TK_CLOSE;
+
+    if(k == TK_W) {
+      --y;
+    } else if(k == TK_S) {
+      ++y;
+    } else if(k == TK_A) {
+      --x;
+    } else if(k == TK_D) {
+      ++x;
+    }
+  }
 
   terminal_close();
 
